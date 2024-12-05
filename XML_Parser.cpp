2de7c283836367ad413a45d6ad0dff1bbcdde38f
GarfieldGod <1351763110@qq.com>
2024-12-05 23:22:28 +0800

    version 0.0.3
    Signed-off-by: GarfieldGod <1351763110@qq.com>

--- XML_Parser.cpp
+++ XML_Parser.cpp
@@ -11,7 +11,25 @@ const char FORWARD_SLASH = '/';
 const char SPACE = ' ';
 const char EQUAL_SIGN = '=';
 const char QUOTATION_SIGN = '\"';
+const char UNDERLINE = '_';
+const char COLON = ':';
 const string WHITE_SPACE = " \t\r\n";
+
+enum TagType {
+    TAG_DEFAULT,
+    TAG_SINGLE,
+    TAG_DOUBLE_STRUCT,
+    TAG_DOUBLE_CONTENT
+};
+
+struct Tag
+{
+    string tagName;
+    vector<string> tagProperty;
+    TagType type;
+    string content;
+    int height;
+};
 
 class XmlParser
 {
@@ -20,14 +38,16 @@ public:
     ~XmlParser();
 
     string readXmlFileToString(string filePath);
-    void ParserXml(string Text);
+    void ParserXml(string text);
+    void printTreeStruct();
 
 private:
     string getTagName(string xml);
-    string getTagProperty(string xml);
+    vector<string> getTagProperty(string xml);
 
     int hight_ = 0;
     vector<string> tagProperty_ {"id", "data"};
+    vector<Tag> tags_;
 };
 
 XmlParser::XmlParser() {}
@@ -53,80 +73,68 @@ string XmlParser::readXmlFileToString(string filePath)
     return result;
 }
 
-void XmlParser::ParserXml(string Text)
+void XmlParser::ParserXml(string text)
 {
-    for (int i = 0; i < Text.size(); i++)
+    for (int i = 0; i < text.size(); i++)
     {
-        if (Text[i] == LEFT_ANGLE_BRACKET)
+        if (text[i] == LEFT_ANGLE_BRACKET)
         {
-            for (int t = 1; t < Text.size()-1; t++)
+            for (int t = 1; t < text.size()-1; t++)
             {
-                if (Text[t] == RIGHT_ANGLE_BRACKET) {
-                    string endstr;
-                    string tagContent = Text.substr(i + 1, t - i - 1);
-                    endstr.push_back(LEFT_ANGLE_BRACKET);
-                    endstr.push_back(FORWARD_SLASH);
-                    endstr += tagContent;
-                    endstr.push_back(RIGHT_ANGLE_BRACKET);
-                    string externinfo = getTagProperty(tagContent);//额外信息
-                    if (externinfo != "")
+                if (text[t] == RIGHT_ANGLE_BRACKET) {
+                    string tagHead = text.substr(i + 1, t - i - 1);
+                    string tagName = getTagName(tagHead);
+                    vector<string> tagProperty = getTagProperty(tagHead);
+                    TagType tagType = TAG_DEFAULT;
+                    string content = "";
+                    //单标签
+                    if (text[t - 1] == FORWARD_SLASH)
                     {
-                        tagContent = getTagName(tagContent);
-                    }
-                    if (Text[t - 1] == FORWARD_SLASH)//单标签
-                    {
-                        string d;
-                        for (int n = 0; n < hight_; n++)
+                        tagType = TAG_SINGLE;
+                        Tag tag = {tagName, tagProperty, tagType, content, hight_};
+                        tags_.push_back(tag);
+
+                        text = text.substr(t + 1, text.size() - 1);
+                        ParserXml(text);
+                    //双标签
+                    } else {
+                        string tagEnd;
+                        tagEnd.push_back(LEFT_ANGLE_BRACKET);
+                        tagEnd.push_back(FORWARD_SLASH);
+                        tagEnd += tagName;
+                        tagEnd.push_back(RIGHT_ANGLE_BRACKET);
+
+                        int tagEndPos = text.find(tagEnd, 0) == -1 ? 0: text.find(tagEnd, 0);
+                        if (tagHead[0] == FORWARD_SLASH)
                         {
-                            d += "\t";
+                            text = text.substr(t + 1,text.size() - 1);
+                            ParserXml(text); break;
                         }
-                        cout << d << tagContent + externinfo<< "\n";
-                        Text = Text.substr(t + 1, Text.size() - 1);
-                        ParserXml(Text);
-                        break;
-                    }
-                    int c = Text.find(endstr, 0) == -1 ? 0: Text.find(endstr, 0);
-                    if (tagContent[0] == FORWARD_SLASH)
-                    {
-                        Text = Text.substr(t+1,Text.size()-1);
-                        ParserXml(Text); break;
-                    }
-                    if (Text[t + 1] == LEFT_ANGLE_BRACKET)//无内容
-                    {
-                        if (Text.find(endstr, 0) != t + 1)
-                        {
-                            //cout << "发现树点：" << Text << "\t\t\t高度：" << hight_ << "\n";
-                            string d;
-                            for (int n = 0; n < hight_; n++)
-                            {
-                                d += "\t";
+                        //无内容标签
+                        if (text[t + 1] == LEFT_ANGLE_BRACKET) {
+                            if (text.find(tagEnd, 0) != t + 1) {
+                                tagType = TAG_DOUBLE_STRUCT;
+                                Tag tag = {tagName, tagProperty, tagType, content, hight_};
+                                tags_.push_back(tag);
+
+                                text = text.substr(t + 1, text.size() - 1);
+                                hight_++;
                             }
-                            cout << d << tagContent+externinfo <<"：" << "\n";
-                            Text = Text.substr(t + 1, Text.size() - 1);
-                            hight_ += 1;
-                            //cout << "裁剪树点：" << Text << "\t\t\t高度：" << hight_ << "\n";
-                            ParserXml(Text);
-                        }
-                        break;
-                    }
-                    else//有内容
-                    {
-                        if (Text.find(endstr, 0) != t + 1)
-                        {
-                            string b = Text.substr(t + 1, Text.find(endstr, 0) - t - 1);
-                            //cout << "找到" << endstr << "的值为：" << b << "\t\t\t高度：" << hight_ << "\n";
-                            string d;
-                            for (int n = 0; n < hight_; n++)
-                            {
-                                d += "\t";
+                        //有内容标签
+                        } else {
+                            if (text.find(tagEnd, 0) != t + 1) {
+                                content = text.substr(t + 1, text.find(tagEnd, 0) - t - 1);
+                                tagType = TAG_DOUBLE_CONTENT;
+                                Tag tag = {tagName, tagProperty, tagType, content, hight_};
+                                tags_.push_back(tag);
+
+                                if (text[text.find(tagEnd, 0) + tagEnd.size() + 1] == FORWARD_SLASH) { hight_--; }
+                                text = text.substr(tagEndPos + tagEnd.size(), text.size() - 1);
                             }
-                            cout <<d<<tagContent+externinfo << "：" << b << "\n";
-                            if (Text[Text.find(endstr, 0) + endstr.size() + 1] == FORWARD_SLASH) { hight_ -= 1; }//Text = Text.substr(c, Text.size() - 1);
-                            Text = Text.substr(c+endstr.size(), Text.size() - 1);
-                            ParserXml(Text);
                         }
-                        break;
+                        ParserXml(text);
                     }
+                    break;
                 }
             }
             break;
@@ -134,50 +142,58 @@ void XmlParser::ParserXml(string Text)
     }
 }
 
-string XmlParser::getTagName(string xml)
+void XmlParser::printTreeStruct()
 {
-    int ss = 0;
-    int ee = 0;
-    for (int i = 0; i < xml.size(); i++)
-    {
-        if (xml[i] != LEFT_ANGLE_BRACKET && xml[i] != SPACE)
-        {
-            ss = i;
+    for(Tag tag : tags_) {
+        string offset;
+        for (int n = 0; n < tag.height; n++) {offset += "\t";}
+        string properties;
+        for (string property : tag.tagProperty) {
+            if (property != "") {
+                properties += UNDERLINE + property;
+            }
+        }
+        cout << offset << tag.tagName + properties + COLON << tag.content << "\n";
+    }
+}
+
+string XmlParser::getTagName(string text)
+{
+    int startPos = 0, endPos = 0;
+    for (int i = 0; i < text.size(); i++) {
+        if (text[i] != LEFT_ANGLE_BRACKET && text[i] != SPACE) {
+            startPos = i;
             break;
         }
     }
-    for (int i = ss; i < xml.size(); i++)
-    {
-        if (xml[i] == SPACE)
-        {
-            ee = i;
+    for (int i = startPos + 1; i <= text.size(); i++) {
+        if (i == text.size() || text[i] == SPACE) {
+            endPos = i;
+            break;
         }
     }
-    return xml.substr(ss, ee - ss);
+    return text.substr(startPos, endPos - startPos);
 }
 
-string XmlParser::getTagProperty(string text)
+vector<string> XmlParser::getTagProperty(string text)
 {
-    vector<string> propertys(tagProperty_.size());
+    vector<string> properties(tagProperty_.size());
     for (int p = 0; p < tagProperty_.size(); p++) {
         string keyStr = tagProperty_[p] + EQUAL_SIGN + QUOTATION_SIGN;
         if (text.find(keyStr, 0) != -1) {
             for (int i = text.find(keyStr, 0) + keyStr.size(); i < text.size(); i++) {
                 if (text[i] == QUOTATION_SIGN) break;
-                else propertys[p].push_back(text[i]);
+                else properties[p].push_back(text[i]);
             }
         }
     }
-    string result;
-    for(string property : propertys) {
-        result += property;
-    }
-    return result;
+    return properties;
 }
 
 int main()
 {
     XmlParser xml;
-    string Text = xml.readXmlFileToString("test/test0.xml");
-    xml.ParserXml(Text);
+    string text = xml.readXmlFileToString("test/test0.xml");
+    xml.ParserXml(text);
+    xml.printTreeStruct();
 }
