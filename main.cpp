cce5944a8bab6b842c28222d763aeda79fddda30
GarfieldGod <1351763110@qq.com>
2024-12-06 15:49:33 +0800

    Use zhml as a tool
    Signed-off-by: GarfieldGod <1351763110@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "zhml_parser.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    ZhmlParser zhml("test/test.zhml");
+    string filePath = "test/test.zhml";
+    if (argc >= 2) {filePath = argv[1];}
+    else {cerr << "Usage: zhml <file_path>" << endl;return 0;}
+    int pos = filePath.find_last_of('.');
+    string fileFormat = filePath.substr(pos + 1, filePath.size() - 1 - pos);
+    if (fileFormat != "zhml") {
+        cerr << "ZhmlParser can only parse .zhml file !" << endl;
+        return 0;
+    }
+
+    ZhmlParser zhml(filePath);
     zhml.printTreeStruct();
     vector<Tag> tags = zhml.getZhmlTags();
 }
